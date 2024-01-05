5becc7538579406a25efb965ca0cbc9c22c99cab
pranitlama <pranitlama211@gmail.com>
2024-01-05 18:52:19 +0545

    scoring and howtoplay

--- mains.js
+++ mains.js
@@ -5,42 +5,26 @@ let btn = document.getElementById("btn");
 let dicerolled = false;
 let totaldice = 0;
 let gameover = false;
+let result = document.getElementById("result");
 
 let diceimg = document.querySelectorAll("img");
+
+let popup = document.getElementById("popup");
+let howto = document.getElementById("howtoplay");
 
 // const isalivemap = new Map();
 
 let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const options = new Map([
-  [0, 0],
-  [1, 1],
-  [2, 2],
-  [3, 3],
-  [4, 4],
-  [5, 5],
-  [6, 6],
-  [7, 7],
-  [8, 8],
-  [9, 9],
-]);
-
 box.forEach((option) => {
   option.addEventListener("click", function () {
     if (dicerolled == true) {
-      // score();
-
       let x = parseInt(option.value);
       let y = option;
-      // isalive(x);
-
-      // if (isalive(totaldice) == false) {
-      //   gameover = true;
-      // }
 
       checkotn(x, y);
     } else {
-      alert("please roll the dice first");
+      // alert("please roll the dice first");
     }
   });
 });
@@ -50,28 +34,31 @@ function checkotn(x, y) {
     alert("the value is more than the dice");
   } else {
     totaldice -= x;
-    y.disabled = true;
-    y.classList.add("clicked");
+    if (!gameover) {
+      y.disabled = true;
+      y.classList.add("clicked");
+    }
     arr.splice(arr.indexOf(x), 1);
-    options.delete(x);
+
     if (isalive(totaldice) == false) {
       gameover = true;
     }
     if (gameover) {
-      alert("gameover");
+      // alert("gameover");
+      openpopup();
     }
     // console.log("option value", totaldice, arr);
   }
   if (totaldice == 0) {
-    alert("please roll dice again !!!");
+    // alert("please roll dice again !!!");
     dicerolled = false;
-  } else {
-    alert("select again");
+  } else if (!gameover) {
+    // alert("select again");
   }
 }
 
 function isalive(totaldice) {
-  if (options.has(totaldice)) {
+  if (arr.includes(totaldice)) {
     return true;
   } else {
     for (let i = 0; i < arr.length - 2; i++) {
@@ -102,18 +89,58 @@ function roll() {
       diceimg.forEach(function (die) {
         die.classList.remove("shake");
       });
-      let d_one = Math.floor(Math.random() * 6) + 1;
-      let d_two = Math.floor(Math.random() * 6) + 1;
+      // let d_one = Math.floor(Math.random() * 6) + 1;
+      // let d_two = Math.floor(Math.random() * 6) + 1;
+
+      let d_one = 5;
+      let d_two = 5;
 
       diceo.src = "/img" + "/" + "d" + d_one + ".png";
       dicet.src = "/img" + "/" + "d" + d_two + ".png";
       totaldice = d_one + d_two;
       dicerolled = true;
-    }, 1000);
-    if (isalive(totaldice) == false) {
-      gameover = true;
-    }
+      if (isalive(totaldice) == false) {
+        // gameover = true;
+        openpopup();
+      }
+    }, 800);
 
     // console.log("total dice", totaldice, options);
   }
+}
+
+function openpopup() {
+  popup.classList.add("open-popup");
+  if (arr.length == 1) {
+    // console.log("win");
+    result.innerHTML = "You Win";
+  } else {
+    // console.log("lose");
+
+    let point = 0;
+    for (let i = 1; i < arr.length; i++) {
+      point += arr[i];
+    }
+    result.innerHTML = "Your point :" + point;
+  }
+}
+function closepopup() {
+  popup.classList.remove("open-popup");
+  arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  dicerolled = false;
+  totaldice = 0;
+  gameover = false;
+
+  box.forEach((option) => {
+    option.disabled = false;
+    option.classList.remove("clicked");
+  });
+}
+
+function openhow() {
+  howto.classList.add("open-popup");
+}
+function closehow() {
+  howto.classList.remove("open-popup");
 }
